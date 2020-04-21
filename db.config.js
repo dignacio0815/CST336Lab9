d8bcbb71d01283d6e80265d79741e850653634ca
EC2 Default User <ec2-user@ip-172-31-20-2.us-east-2.compute.internal>
2020-04-21 04:46:42 +0000

    added db

--- db.config.js
+++ db.config.js
@@ -6,3 +6,4 @@ module.exports = {
 };
 
 //  mysql://b473ff65a7ffb2:1449f782@us-cdbr-iron-east-01.cleardb.net/heroku_1d8a9ad6b1fca3b?reconnect=true
+// mysql --host=us-cdbr-iron-east-01.cleardb.net --user=b473ff65a7ffb2 --password=1449f782 --reconnect heroku_1d8a9ad6b1fca3b < sql/quotes-db.sql
